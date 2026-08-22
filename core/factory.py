5cf9290d212d5d521f0ecb7044dc3c1a2637dd48
ak811 <alikrahimian@gmail.com>
2026-08-22 01:54:33 -0400

    Add fibottention_sparse backend to factory.py

--- core/factory.py
+++ core/factory.py
@@ -1,25 +1,23 @@
-# core/factory.py
 from .attention import AttentionDispatcher
 
-# >>> add these blocks so decorators run once <<<
 # Backends
-from .backends import vit as _b_vit                 # registers "vit"
-from .backends import efficient as _b_eff           # registers "efficient"
-from .backends import linformer as _b_lin           # registers "linformer"
-from .backends import vit_qchunk as _b_vit_qchunk   # registers "vit_qchunk"
-from .backends import vit_qchunk_topk as _b_vit_topk  # registers "vit_topk"
+from .backends import vit as _b_vit                  
+from .backends import efficient as _b_eff            
+from .backends import linformer as _b_lin            
+from .backends import vit_qchunk as _b_vit_qchunk    
+from .backends import vit_qchunk_topk as _b_vit_topk 
 from .backends import efficient_local as _b_eff_local
+from .backends import fibottention_sparse as _b_fibo_sparse 
 
 # Masks
-from .masks import none as _m_none                  # registers "none"
-from .masks import sparse as _m_sparse              # registers "sparse"
-from .masks import fibottention as _m_fibo          # registers "fibottention"
-from .masks import fibottention_crossdiag as _m_fibo_xdiag  # registers "fibottentioncrossdiag"
+from .masks import none as _m_none                  
+from .masks import sparse as _m_sparse              
+from .masks import fibottention as _m_fibo          
+from .masks import fibottention_crossdiag as _m_fibo_xdiag  
 from .masks import topk as _m_topk
 from .masks import random as _m_random
-from .masks import bigbird as _m_bigbird            # registers "bigbird"
+from .masks import bigbird as _m_bigbird            
 from .masks import longformer as _m_longformer
-# <<< end add >>>
 
 from .backends.linformer import LinformerProjector
 
